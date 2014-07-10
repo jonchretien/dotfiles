8667ebf99f32b10c2469616d194e8e6e248aad14
Jon Chretien <jchretien@10.22.32.41.office.spotify.net>
2014-07-10 11:18:31 -0400

    backup old dotfiles & add symlinks to new ones

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,35 @@
 #!/usr/bin/env bash
 
+###############################################################################
+# Variables
+###############################################################################
+
+# directory paths
+BACKUP_DIR="dotfile_backup"
+CURRENT_DIR=$(pwd)
+
+###############################################################################
+# Functions
+###############################################################################
+
+create_backup_dir() {
+  echo "Creating backup directory."
+  mkdir -p ~/$BACKUP_DIR
+}
+
+install_dotfiles() {
+  echo "Archiving old dotfiles and creating symlinks for new ones."
+  files=( '.aliases' '.bash_profile' '.bash_prompt' '.bashrc' '.editorconfig' '.exports' '.functions' '.gitconfig' '.gitignore' '.gvimrc' '.vimrc' )
+
+  for file in "${files[@]}"
+  do
+     mv ~/$file ~/$BACKUP_DIR/$file
+     ln -s $CURRENT_DIR/$file ~/$file
+  done
+}
+
 install_npm_modules() {
-  echo "Installing global npm modules"
+  echo "Installing global npm modules."
   npm install -g bower
   npm install -g browserify
   npm install -g grunt-cli
@@ -12,12 +40,12 @@ install_npm_modules() {
 }
 
 install_ruby_gems() {
-  echo "Ruby gems"
+  echo "Installing ruby gems."
   gem install sass
 }
 
 open_tabs() {
-  echo 'Opening links to apps:'
+  echo 'Opening links to apps.'
   echo '>> Chrome'
   open https://www.google.com/intl/en/chrome/browser/
   echo '>> Chrome Canary'
@@ -45,11 +73,25 @@ open_tabs() {
 }
 
 install_homebrew() {
-  echo 'Installing Homebrew, MySQL, and Node.js...'
+  echo 'Installing Homebrew, MySQL, and Node.js.'
   ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
   brew update
   brew install mysql node
 }
+
+###############################################################################
+# User Prompts
+###############################################################################
+
+echo 'Should I create a folder to backup your current dotfiles?'
+echo 'y/n'
+read prompt_backup
+[[ "$prompt_backup" == 'y' ]] && create_backup_dir
+
+echo 'Should I create symlinks to the new dotfiles?'
+echo 'y/n'
+read prompt_symlinks
+[[ "$prompt_symlinks" == 'y' ]] && install_dotfiles
 
 echo 'Should I open tabs for system applications (e.g. Chrome, Dropbox, etc...)?'
 echo 'y/n'
